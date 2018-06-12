f719be84775cfb1b8b9d637f037287115d256425
irrino4ka <ir.rino4ka@gmail.com>
2018-06-12 14:47:30 +0300

    inline style, added sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from './../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxiliary>
+        <SideDrawer />
         <Toolbar/>
         <main className = {classes.Content}>
             {props.children}
